6f86be69a48a8c840f1588edccea041b7c0d9a29
ragu795 <ragu795@gmail.com>
2019-08-14 01:08:43 +0530

    Iterative approach

--- Tree/find_closest_value_bst.py
+++ Tree/find_closest_value_bst.py
@@ -16,23 +16,19 @@ class tree(object):
         self.right = None
         self.value = value
 
-def findClosestValue(t, target, closest):
-    if t is None:
-        return closest
-    if t.value == target:
-        return t.value
-    if abs(target-closest) > abs(target-t.value):
-        closest = t.value
-    if target < t.value:
-        return findClosestValue(t.left, target, closest)
-    elif target > t.value:
-        return findClosestValue(t.right, target, closest)
-    else:
-        return t.value
-
 def findClosestValueInBst(t, target):
-    return findClosestValue(t, target, float('inf'))
-                   
+    closest = float("inf")
+    current_node = t
+    while current_node is not None:
+        if abs(target-closest) > abs(target-current_node.value):
+            closest = current_node.value
+        if target < current_node.value:
+            current_node = current_node.left
+        elif target > current_node.value:
+            current_node = current_node.right
+        else:
+             break  
+    return closest
 
 if __name__ == '__main__':
     t = tree(10)
@@ -44,6 +40,5 @@ if __name__ == '__main__':
     t.right.right = tree(22)
     t.left.left.left = tree(1)
     t.right.left.left = tree(14)
-    print t.left.value
     print findClosestValueInBst(t, 12)
     
