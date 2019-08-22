6ab5de15d323c178d8b9c72a97ab4cb9e1767fd9
raguram.gopi <raguram.gopi@global-analytics.com>
2019-08-23 00:47:02 +0530

    Nth fibonacii with memoization

--- Recursion/nth_fibonacii_number.py
+++ Recursion/nth_fibonacii_number.py
@@ -5,14 +5,19 @@ Write a function that takes in an integer n and returns the nth Fibonacci number
 """
 
 
-def nth_fibonacii_number(n):
+def nth_fibonacii_number(n, fib):
     if n == 1:
+        fib[n] = 0
         return 0
     if n == 2:
+        fib[n] = 1
         return 1
+    if fib[n]:
+        return fib[n]
+    fib[n] = nth_fibonacii_number(n - 1, fib) + nth_fibonacii_number(n - 2, fib)
+    return fib[n]
 
-    return nth_fibonacii_number(n - 1) + nth_fibonacii_number(n - 2)
 
-
-n = 19
-print nth_fibonacii_number(n)
+n = 6
+fib = [0] * (n + 1)
+print nth_fibonacii_number(n, fib)
