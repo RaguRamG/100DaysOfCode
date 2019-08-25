0469076f4aee8ccbbdecddd64e971ab774afa0e1
raguram.gopi <raguram.gopi@global-analytics.com>
2019-08-26 01:01:43 +0530

    powerset iterative solution

--- Recursion/powerset.py
+++ Recursion/powerset.py
@@ -1,16 +1,10 @@
-def powerset(array, idx=None):
-    if idx is None:
-        idx = len(array) - 1
-    elif idx < 0:
-        return [[]]
-    ele = array[idx]
-    subsets = powerset(array, idx - 1)
-    print subsets, "ele: ", ele
-    for i in range(len(subsets)):
-        currentSubset = subsets[i]
-        subsets.append(currentSubset + [ele])
+def powerset(array):
+    subsets = [[]]
+    for ele in array:
+        for i in range(len(subsets)):
+            currSubset = subsets[i] + [ele]
+            subsets.append(currSubset)
     return subsets
-
 
 output = powerset([1, 2, 3, 4])
 print output
